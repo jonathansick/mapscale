a519ec251e8a0351eae9064b2fba80ebdd958dc5
Jonathan Sick <jonathansick@mac.com>
2012-05-13 16:57:01 -0400

    Add simple burn-in test

---
+++ mapscale/test.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+Basic burn-in test.
+"""
+
+import time
+
+from mapscale import Processor
+
+
+def main():
+    myfunc = WorkFunction()
+    mapper = Processor(myfunc, 1)
+    mapper.shutdown()
+    # jobList = range(5)
+    # results = mapper(jobList)
+    # print results
+
+
+class WorkFunction(object):
+    """A function that we want to compute in parallel."""
+    def __init__(self):
+        super(WorkFunction, self).__init__()
+
+    def __call__(self, x):
+        """docstring for __call__"""
+        print x
+        time.sleep(1.)
+        return x ** 2.
+
+    def setup(self):
+        """docstring for setup"""
+        print "Setting up"
+
+    def cleanup(self):
+        """docstring for cleanup"""
+        print "Cleaning up"
+
+if __name__ == '__main__':
+    main()
